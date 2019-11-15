c62d1757fe6e8fa4b54d55f8b5eec53ceffe5fee
jackcornellober <jackcornellober@gmail.com>
2019-11-14 16:11:07 -0800

    done with mon-tues MVP

--- ls8/cpu.py
+++ ls8/cpu.py
@@ -2,41 +2,44 @@
 
 import sys
 
+
 class CPU:
     """Main CPU class."""
 
     def __init__(self):
         """Construct a new CPU."""
-        pass
+        self.ram = [0] * 256
+        self.register = [0] * 8
+        self.pc = 0
 
     def load(self):
-        """Load a program into memory."""
 
+        if len(sys.argv) != 2:
+            print("usage: comp.py filename")
+            sys.exit(1)
+        progname = sys.argv[1]
+            
         address = 0
 
-        # For now, we've just hardcoded a program:
+        with open(progname) as f:
+            for line in f:
+                line = line.split("#")[0]
+                line = line.strip()
+                if line == '':
+                    continue
+                
 
-        program = [
-            # From print8.ls8
-            0b10000010, # LDI R0,8
-            0b00000000,
-            0b00001000,
-            0b01000111, # PRN R0
-            0b00000000,
-            0b00000001, # HLT
-        ]
+                val = int(line,2)
 
-        for instruction in program:
-            self.ram[address] = instruction
-            address += 1
-
+                self.ram[address] = val
+                address += 1
 
     def alu(self, op, reg_a, reg_b):
         """ALU operations."""
 
         if op == "ADD":
             self.reg[reg_a] += self.reg[reg_b]
-        #elif op == "SUB": etc
+        # elif op == "SUB": etc
         else:
             raise Exception("Unsupported ALU operation")
 
@@ -48,8 +51,8 @@ class CPU:
 
         print(f"TRACE: %02X | %02X %02X %02X |" % (
             self.pc,
-            #self.fl,
-            #self.ie,
+            # self.fl,
+            # self.ie,
             self.ram_read(self.pc),
             self.ram_read(self.pc + 1),
             self.ram_read(self.pc + 2)
@@ -62,4 +65,41 @@ class CPU:
 
     def run(self):
         """Run the CPU."""
-        pass
+        self.load()
+        
+        halted = False
+        self.pc = 0
+        while not halted:
+            print('pc', self.pc)
+            instruction = self.ram_read(self.pc)
+
+            
+            if instruction == 0b10000010:
+                self.register[self.ram_read(self.pc+1)] = self.ram_read(self.pc+2)
+                self.pc += 3
+            elif instruction == 0b01000111:
+                print(self.register[self.ram_read(self.pc+1)])
+                self.pc += 2
+            
+            elif instruction == 0b10100010:
+                self.register[self.ram_read(self.pc+1)] = self.register[self.ram_read(self.pc+1)] * self.register[self.ram_read(self.pc+2)]
+                self.pc += 3
+
+            elif instruction == 1:
+            	halted = True
+            	self.pc = 0            
+
+
+
+        
+
+    def ram_read(self, address):
+        return self.ram[address]
+
+    def ram_write(self, address, value):
+        self.ram[address] = value
+        return
+
+cpu = CPU()
+
+cpu.run()
